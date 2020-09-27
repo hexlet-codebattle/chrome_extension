f7fb05f0e908b9be6f62e1c2e5e32111364301bb
BabaevEldar <36798811+BabayevEldar@users.noreply.github.com>
2020-09-27 19:26:58 +0700

    H9ko 16 refactor with rx js (#22)
    * add RxJS
    
    * refactor_with_RsJS
    
    * change global state on local app state
    
    * Changed state management
    
    Co-authored-by: Vova Afan <grmh9ko@gmail.com>

---
+++ src/background/browser-actions.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+
+import browser from 'webextension-polyfill';
+
+const setBadge = number => (number > 0
+  ? browser.browserAction.setBadgeText({ text: `${number}` })
+  : browser.browserAction.setBadgeText({ text: null }));
+
+const stopFlashingBadge = id => {
+  clearInterval(id);
+  browser.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+};
+const flashBadge = () => {
+  let flashing = true;
+  const flash = () => {
+    if (flashing) {
+      browser.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+    } else {
+      browser.browserAction.setBadgeBackgroundColor({ color: '#FF0000' });
+    }
+    flashing = !flashing;
+  };
+  const badgeFlashTimerId = setInterval(flash, 500);
+  return badgeFlashTimerId;
+};
+
+const animateBadge = (timeout = 10000) => {
+  const timerId = flashBadge();
+  setTimeout(() => {
+    console.log('animateBadge -> setTimeout', setTimeout);
+    stopFlashingBadge(timerId);
+  }, timeout);
+};
+export { setBadge, flashBadge, animateBadge };
