bdded5f269855d6a3d8f8e30178eb0af114d3a0f
Vova Afan <grmh9ko@gmail.com>
2020-10-11 17:57:12 +0300

    add defaultStorage.js

---
+++ src/options/defaultStorage.js
@@ -0,0 +1,13 @@
+const defaultStorage = {
+  toggles: {
+    flashing: true,
+    showNotifications: {
+      newGame: true,
+      newTournament: true,
+      opponentJoin: true,
+    },
+  },
+  popupTheme: 'white',
+};
+
+export default defaultStorage;
