80638167fd003371e724eaa69595e1ec3d6b4f48
Vova Afan <grmh9ko@gmail.com>
2020-10-11 18:07:10 +0300

    remake options page

--- src/options/reducer/index.js
+++ src/options/reducer/index.js
@@ -1,13 +1,26 @@
-import { setTheme, setToggle } from '../actions';
+import { setShowNotification, setTheme, setToggle } from '../actions';
 
 export default (state, action) => {
+  console.log('%c%s', 'color: #733d00', JSON.stringify(action));
+  console.log('%c%s', 'color: #e50000', JSON.stringify(state));
   switch (action.type) {
     case setToggle:
       return {
         ...state,
         toggles: {
-          ...state.toogles,
+          ...state.toggles,
           ...action.payload,
+        },
+      };
+    case setShowNotification:
+      return {
+        ...state,
+        toggles: {
+          ...state.toggles,
+          showNotifications: {
+            ...state.toggles.showNotifications,
+            ...action.payload,
+          },
         },
       };
     case setTheme:
